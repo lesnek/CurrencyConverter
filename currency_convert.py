81810e3443091cab8ff80c0c76f5d717e64b6f67
lesnek <lesnek@eurosat.cz>
2018-10-16 13:52:29 +0200

    Change urllib to requests library

--- currency_convert.py
+++ currency_convert.py
@@ -1,22 +1,21 @@
 
-import urllib.request
+import requests
 import json
 import sys
 
 class CurrencyConverter:
-    def __init__(self, url = "https://api.exchangeratesapi.io/latest"):
-        req  = urllib.request.Request(url) 
-        data = urllib.request.urlopen(req).read()
-        data = json.loads(data.decode('utf-8'))
-        self.rates = data["rates"]
+    def __init__(self, url="https://api.exchangeratesapi.io/latest"):
+        req  = requests.get(url) 
+        jsonData = req.json()
+        self.rates = jsonData["rates"]
 
 
     # Converting currency by EUR rates
-    def convert(self, amount, from_c, to_c="none"):
+    def convert(self, amount, from_c, to_c=None):
         result_arr = dict()
         if from_c != "EUR":
             amount = amount / self.rates[from_c]
-        if (to_c == "none"):
+        if (to_c == None):
             for key in self.rates:
                 converted = amount * self.rates[key]
                 result_arr[key] = converted
