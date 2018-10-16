62d0f1c1d3176299a53fff74fb78c342c9f4f416
lesnek <lesnek@eurosat.cz>
2018-10-16 15:08:18 +0200

    output CLI formatter

--- currency_convert.py
+++ currency_convert.py
@@ -8,24 +8,32 @@ class CurrencyConverter:
         req  = requests.get(url) 
         jsonData = req.json()
         self.rates = jsonData["rates"]
+        self.result_dict = dict()
 
 
     # Converting currency by EUR rates
     def convert(self, amount, from_c, to_c=None):
-        result_arr = dict()
         if from_c != "EUR":
             amount = amount / self.rates[from_c]
         if (to_c == None):
             for key in self.rates:
                 converted = amount * self.rates[key]
-                result_arr[key] = converted
-            return result_arr
+                self.result_dict[key] = converted
         else:
             if to_c == "EUR":
-                return amount
+                self.result_dict["EUR"] = amount
             else:
-                return amount * self.rates[to_c]
+                self.result_dict[to_c] = amount * self.rates[to_c]
+        return self.printFormatter(amount, from_c, self.result_dict)
+
+    # Finish the output format
+    def printFormatter(self, amount, input_currency, result):
+        output = dict()
+        output["input"] = {"amount":amount, "currency":input_currency}
+        output["output"] = result
+        return output
+
 
 converter = CurrencyConverter()
 
-print(converter.convert(100000, "CZK"))
+print(converter.convert(100, "CZK", "EUR"))
