085947264b095dcac9ed61ea7d6a6aa422b26bef
lesnek <lesnek@eurosat.cz>
2018-10-17 15:18:44 +0200

    Converter fix, Readme update

--- src/currency_convert.py
+++ src/currency_convert.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3.6
 
 import json
 import sys
@@ -20,14 +21,14 @@ class CurrencyConverter:
         :param str to_currency: Requested currency - 3 letters currency symbol
         '''
         result_dict = {}
-        if from_currency != "EUR":
+        if (from_currency != "EUR"):
             amount = amount_input / self.rates[from_currency]
         if (to_currency == None):
             for key in self.rates:
                 converted = amount * self.rates[key]
                 result_dict[key] = converted
         else:
-            if to_currency == "EUR":
+            if (to_currency == "EUR"):
                 result_dict["EUR"] = amount
             else:
                 result_dict[to_currency] = amount * self.rates[to_currency]
@@ -48,8 +49,8 @@ class CurrencyConverter:
 
     def cli_input(self):
         parser = argparse.ArgumentParser(description='Currency converter')
-        parser.add_argument('--amount', type=int, help="Amount which we want to convert")
-        parser.add_argument('--input_currency', help="Input currency in format of 3 letters symbol eg.(EUR)")
+        parser.add_argument('--amount', type=int, required=True, help="Amount which we want to convert")
+        parser.add_argument('--input_currency', required=True, help="Input currency in format of 3 letters symbol eg.(EUR)")
         parser.add_argument('--output_currency', default=None, help="Output currency in format of 3 letters symbol eg.(USD)")
         args = parser.parse_args()
         result = self.convert(args.amount, args.input_currency, args.output_currency)
